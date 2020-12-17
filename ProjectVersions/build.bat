0045edc1fb96328084a12204cec9f08c4f29f7c2
Mirek <Mirek@DESKTOP-PADK6JG>
2020-12-17 03:32:02 -0800

    fix 2

--- ProjectVersions/build.bat
+++ ProjectVersions/build.bat
@@ -9,8 +9,21 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-REM Build
-"%programfiles(x86)%\MSBuild\15.0\Bin\MSBuild.exe" ProjectVersions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+REM Detect MSBuild 15.0 path
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\15.0\Bin\MSBuild.exe"
+REM %msbuild%
+)
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+REM %msbuild%
+)
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+REM %msbuild%
+)
+
+REM (optional) build.bat is in the root of our repo, cd to the correct folder where sources/projects are
 
 REM Package
 mkdir Build
