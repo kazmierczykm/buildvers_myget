e295f9828b08fea7aa00929a128fac71ad132dca
Mirek <Mirek@DESKTOP-PADK6JG>
2020-12-17 02:43:17 -0800

    corrected

--- ProjectVersions/build.bat
+++ ProjectVersions/build.bat
@@ -4,6 +4,17 @@ if "%config%" == "" (
    set config=Release
 )
 
-set version-Version %PackageVersion%
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ProjectVersions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+call %nuget% pack "ProjectVersions\ProjectVersions.csproj" -symbols -o Build -p Configuration=%config% %version%
+
 
 
